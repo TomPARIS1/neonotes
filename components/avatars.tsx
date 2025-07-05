406a69d5d01296dc06e4415197d0ee34165229a0
Tom <98466837+TomPARIS1@users.noreply.github.com>
2025-07-05 19:37:06 +0200

    Added avatar of users when editing

--- components/avatars.tsx
+++ components/avatars.tsx
@@ -1,6 +1,13 @@
 'use client'
 
 import { useOthers, useSelf } from "@liveblocks/react/suspense"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function Avatars() {
     const others = useOthers();
@@ -13,7 +20,21 @@ function Avatars() {
         <p className="font-light text-sm">Utilisateurs éditant cette page</p>
 
         <div className="flex -space-x-5">
-
+            {all.map((other, i) => (
+                <TooltipProvider key={other.id + i}>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Avatar className="border-2 hover:z-50">
+                                <AvatarImage src={other?.info.avatar} />
+                                <AvatarFallback>{other?.info.name}</AvatarFallback>
+                            </Avatar>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>{self?.id === other?.id ? "Vous" : other?.info.name}</p>
+                        </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            ))}
         </div>
     </div>
   )
